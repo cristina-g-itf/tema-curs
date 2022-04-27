d8070c89610cd3447e58efdf582bc8d20a01f482
Cristina G <tabacaru.cristina@gmail.com>
2022-04-27 14:48:14 +0300

    play with CSS

--- tema-1/main.js
+++ tema-1/main.js
@@ -1,2 +1,12 @@
 console.log('Cristina Grigoroiu');
-
+function myFunction() {
+    let person = prompt("Please enter your name");
+    if (person != null) {
+      document.getElementById("name").innerHTML =
+      "Hello " + person + "! I'm Cristina! Here is the town that I live in:";
+    }
+    else{
+         document.getElementById("name").innerHTML =
+      "Hello! I'm Cristina! Here is the town that I live in:";
+    }
+}
