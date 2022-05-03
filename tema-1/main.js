db1fb8088234519ad7ae80c76f19d3e032619c41
Tudor Goia <mindsetcoach360@gmail.com>
2022-05-03 22:10:04 +0300

    fixing home work

--- tema-1/main.js
+++ tema-1/main.js
@@ -1,7 +1,7 @@
 console.log('Cristina Grigoroiu');
 function myFunction() {
     let person = prompt("Please enter your name");
-    if (person != null) {
+    if (person !== "") {
       document.getElementById("name").innerHTML =
       "Hello " + person + "! I'm Cristina! Here is the town that I live in:";
     }
