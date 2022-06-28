79919da0ffc1547dd67786bc405f09e35e943e93
Cristina G <tabacaru.cristina@gmail.com>
2022-06-28 17:35:06 +0300

    a better look at the task

--- tema-9/date.js
+++ tema-9/date.js
@@ -2,14 +2,24 @@ let year = 2021;
 let month = 6;
 let day = 1;
 let dateDiv = document.getElementById('date');
+let divForEachDate = document.createElement('div');
+let dayArray = ['Mon.', 'Tues.', 'Wed.', 'Thur.', 'Fri.', 'Sat.', 'Sun.'];
+let monthArray = ['January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'];
 
 
 for (new Date(year, month, day); new Date(year, month, day) <= new Date(); day += 6) {
 
     let newDate = new Date(year, month, day);
 
-    let text = document.createTextNode(newDate);
-    let divForEachDate = document.createElement('div');
+    let dayOfWeek = dayArray[newDate.getDay()];
+    let dayOfMonth = newDate.getDate();
+    let theMonth = monthArray[newDate.getMonth()];
+    let theYear = newDate.getFullYear();
+    let data = dayOfWeek + ', ' + theMonth + ' ' + dayOfMonth + ', ' + theYear;
+    
+
+    let text = document.createTextNode(data);    
     divForEachDate.appendChild(text);
 
     let newLine = document.createElement('br');
@@ -17,4 +27,5 @@ for (new Date(year, month, day); new Date(year, month, day) <= new Date(); day +
     dateDiv.appendChild(divForEachDate);
     dateDiv.appendChild(newLine);
 }
+
 
